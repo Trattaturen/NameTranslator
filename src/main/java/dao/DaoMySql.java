c6c3d00d9932e576d1e59711b59801ffe7d63695
trattaturen <lebedev.d.y@gmail.com>
2016-11-01 15:31:37 +0200

    implemented worker

---
+++ src/main/java/dao/DaoMySql.java
@@ -0,0 +1,100 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * A singleton class, that provides access to MySQL DB
+ */
+public class DaoMySql
+{
+    final static Logger logger = Logger.getLogger(DaoMySql.class);
+
+    private static DaoMySql daoMySQL;
+
+    private static final String DEFAULT_JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DEFAULT_DB_URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    private static final String DEFAULT_USER = "root";
+    private static final String DEFAULT_PASS = "pass";
+    private static final String ADD_NAMES_QUERY = "INSERT INTO Names(eng_word) VALUES (?);";
+    // private static final String GET_TOTAL_COUNT_QUERY = "SELECT COUNT(*) FROM
+    // Articles;";
+
+    private String driver;
+    private String url;
+    private String user;
+    private String pass;
+
+    /**
+     * @return instance of a class (if it is not yet instantiated - creates it)
+     */
+    public static synchronized DaoMySql getInstance()
+    {
+	if (daoMySQL == null)
+	{
+	    daoMySQL = new DaoMySql();
+	}
+	return daoMySQL;
+    }
+
+    /**
+     * Private constructor of class. Should only be called by getInstance()
+     * method
+     */
+    private DaoMySql() {
+	logger.debug("Initializing MySql Dao with default parameters");
+	driver = DEFAULT_JDBC_DRIVER;
+	url = DEFAULT_DB_URL;
+	user = DEFAULT_USER;
+	pass = DEFAULT_PASS;
+	daoMySQL = this;
+    }
+
+    public void add(List<String> names)
+    {
+	logger.debug("Adding article to MySql");
+	Connection connection = null;
+
+	try
+	{
+	    logger.debug("Getting JDBC driver");
+	    Class.forName(driver);
+	    logger.debug("Getting connection");
+	    connection = DriverManager.getConnection(url, user, pass);
+	    logger.debug("Preparing statement");
+	    PreparedStatement preparedStatement = connection.prepareStatement(ADD_NAMES_QUERY);
+	    logger.debug("Setting statement parameters");
+	    for (String current : names)
+	    {
+		preparedStatement.setString(1, current);
+		preparedStatement.addBatch();
+	    }
+
+	    logger.debug("Executing statement");
+	    preparedStatement.executeUpdate();
+
+	} catch (SQLException se)
+	{
+	    logger.error("Exception while adding to MySql", se);
+
+	} catch (Exception e)
+	{
+	    logger.error("Exception while adding to MySql", e);
+	} finally
+	{
+	    try
+	    {
+		if (connection != null)
+		    connection.close();
+	    } catch (SQLException se)
+	    {
+		logger.error("Exception while closing connection", se);
+	    }
+	}
+    }
+}
---
+++ src/main/java/translator/Worker.java
@@ -0,0 +1,81 @@
+package translator;
+
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.concurrent.Callable;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+/**
+ * Callable that handles translation via Yandex API
+ *
+ */
+public class Worker implements Runnable
+{
+    final static Logger logger = Logger.getLogger(Worker.class);
+
+    private static final int YANDEX_LIMIT_DAILY = 1000000;
+    /// private static final int YANDEX_LIMIT_MONTHLY = 10000000;
+    private int translatedSymbols;
+    private static final String YANDEX_URL = "https://translate.yandex.net/api/v1.5/tr.json/translate?lang=en&key=";
+    private static final String YANDEX_KEY = "trnsl.1.1.20161020T120319Z.1eef5b89221649d3.79a09b2042b8bd60500bbce401514cbe065e930b";
+    private static final MediaType REQUEST_MEDIA_TYPE_JSON = MediaType.parse("application/x-www-form-urlencoded");
+    // Constants, needed to parse request/response
+    private static final String TEXT_KEY_REQUEST = "text=";
+    private static final String TEXT_KEY_RESPONSE = "text";
+
+    private JsonParser jsonParser;
+    private OkHttpClient client;
+
+    public Worker(Manager manager) {
+	logger.debug("Initializing Translator");
+	jsonParser = new JsonParser();
+	// Simple http client library
+	client = new OkHttpClient();
+    }
+
+    @Override
+    public void run()
+    {
+	String toTranslate = manager.getNextWord();
+	while (toTranslate != null)
+	{
+	    translatedSymbols += toTranslate.length();
+	    if (translatedSymbols >= YANDEX_LIMIT_DAILY)
+	    {
+		logger.error("Reached daily translation limit");
+		break;
+	    }
+	    String translated = null;
+	    logger.debug("Creating request body");
+	    String translateRequest = TEXT_KEY_REQUEST + toTranslate;
+	    RequestBody body = RequestBody.create(REQUEST_MEDIA_TYPE_JSON, translateRequest);
+	    logger.debug("Building request");
+	    Request request = new Request.Builder().url(YANDEX_URL + YANDEX_KEY).post(body).build();
+
+	    Response response;
+	    try
+	    {
+		logger.debug("Posting request to yandex");
+		response = client.newCall(request).execute();
+		logger.debug("Parsing response");
+		JsonObject sourceObject = jsonParser.parse(response.body().string()).getAsJsonObject();
+		translated = sourceObject.getAsJsonArray(TEXT_KEY_RESPONSE).get(0).getAsString();
+		logger.debug("Got request from yandex");
+	    } catch (IOException e)
+	    {
+		logger.warn("Exception while translating", e);
+	    }
+	    manager.submitTranslation(toTranslate, translated);
+	}
+
+    }
+}
